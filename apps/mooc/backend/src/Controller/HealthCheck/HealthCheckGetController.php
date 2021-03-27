79ce57032bba61b74c25aa87ff2c1c95817bb92b
Rafael Flores <fita500@gmail.com>
2021-03-27 17:48:03 -0600

    Add health check controller

--- apps/mooc/backend/src/Controller/HealthCheck/HealthCheckGetController.php
+++ apps/mooc/backend/src/Controller/HealthCheck/HealthCheckGetController.php
@@ -4,27 +4,15 @@ declare(strict_types = 1);
 
 namespace CodelyTv\Apps\Mooc\Backend\Controller\HealthCheck;
 
-use CodelyTv\Shared\Infrastructure\RandomNumberGenerator;
+
 use Symfony\Component\HttpFoundation\JsonResponse;
-use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
 
 final class HealthCheckGetController
 {
-    private $generator;
-
-    public function __construct(RandomNumberGenerator $generator)
+    public function __invoke()
     {
-        $this->generator = $generator;
-    }
-
-    public function __invoke(Request $request): Response
-    {
-        return new JsonResponse(
-            [
-                'mooc-backend' => 'ok',
-                'rand'         => $this->generator->generate(),
-            ]
-        );
+        return new JsonResponse([
+            'status'=> 'ok'
+        ]);
     }
 }
