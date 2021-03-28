b5413a1f82acb2d2376d969eacc3a6478e192079
Rafael Flores <fita500@gmail.com>
2021-03-27 18:08:35 -0600

    Add random number generator to health check

--- apps/mooc/backend/src/Controller/HealthCheck/HealthCheckGetController.php
+++ apps/mooc/backend/src/Controller/HealthCheck/HealthCheckGetController.php
@@ -5,14 +5,25 @@ declare(strict_types = 1);
 namespace CodelyTv\Apps\Mooc\Backend\Controller\HealthCheck;
 
 
+use CodelyTv\Shared\Infrastructure\RandomNumberGenerator;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 final class HealthCheckGetController
 {
-    public function __invoke()
+    private $generator;
+
+    public function __construct(RandomNumberGenerator $generator)
+    {
+        $this->generator = $generator;
+    }
+
+    public function __invoke(Request $request): Response
     {
         return new JsonResponse([
-            'status'=> 'ok'
+            'status' => 'ok',
+            'number' => $this->generator->generate()
         ]);
     }
 }
